248df5ff4696302f91253dde3fdd7c742d226b0c
Nico <nicoben.devilliers@gmail.com>
2021-05-30 14:43:13 +0200

    feat: initialize rover object test

---
+++ src/main/java/model/Rover.java
@@ -0,0 +1,29 @@
+package model;
+
+import util.CardinalDirection;
+
+public class Rover {
+
+    private int xCoord;
+    private int yCoord;
+
+    private CardinalDirection bearing;
+
+    public Rover(int xCoord, int yCoord, CardinalDirection bearing) {
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.bearing = bearing;
+    }
+
+    public int getXCoord() {
+        return xCoord;
+    }
+
+    public int getYCoord() {
+        return yCoord;
+    }
+
+    public CardinalDirection getBearing() {
+        return bearing;
+    }
+}
---
+++ src/test/java/model/RoverTest.java
@@ -0,0 +1,25 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import util.CardinalDirection;
+
+public class RoverTest {
+
+    private Rover rover;
+
+    @BeforeEach
+    public void createRover () {
+        rover = new Rover(1, 1, CardinalDirection.NORTH);
+    }
+
+    @Test
+    public void instantiationTest() {
+        Assertions.assertNotNull(rover);
+        Assertions.assertEquals(1, rover.getXCoord());
+        Assertions.assertEquals(1, rover.getYCoord());
+        Assertions.assertEquals(CardinalDirection.NORTH, rover.getBearing());
+    }
+
+}
